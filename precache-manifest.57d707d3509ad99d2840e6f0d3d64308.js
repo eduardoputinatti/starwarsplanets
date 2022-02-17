7fc8a98ac04b77d6593c300874885dd7b5b34711
Luiz Putinatti <luiz_edu16@hotmail.com>
2022-02-17 19:46:26 -0300

    Updates

---
+++ precache-manifest.57d707d3509ad99d2840e6f0d3d64308.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adb03a0e7ab9ba84f7db8f898c575234",
+    "url": "/starwarsplanets/index.html"
+  },
+  {
+    "revision": "a526840b5358b9df8fc9",
+    "url": "/starwarsplanets/static/css/main.0c8dd27f.chunk.css"
+  },
+  {
+    "revision": "0f2e6056ad303b56df1d",
+    "url": "/starwarsplanets/static/js/2.7bcb6336.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/starwarsplanets/static/js/2.7bcb6336.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a526840b5358b9df8fc9",
+    "url": "/starwarsplanets/static/js/main.fb6bcf30.chunk.js"
+  },
+  {
+    "revision": "08db6f37833f80544881",
+    "url": "/starwarsplanets/static/js/runtime-main.d5bfae73.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/starwarsplanets/precache-manifest.57d707d3509ad99d2840e6f0d3d64308.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/starwarsplanets/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
